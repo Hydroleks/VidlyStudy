276518ea99b271b79b4e3db6bfa113bf0d2e07b3
Hydroleks <flexyhandman@msn.com>
2017-10-29 18:21:16 +1100

    Added Genre table with data. Added column with data to Movies. Movies view updated to display the corresponding data.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,11 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -52,7 +65,7 @@ namespace Vidly.Controllers
         //movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
--- Vidly/Models/Genre.cs
+++ Vidly/Models/Genre.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
-    public class Movie
+    public class Genre
     {
-        public int Id { get; set; }
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
-        public Genre Genre { get; set; }
     }
 }
